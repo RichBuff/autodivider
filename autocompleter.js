e19cc4d7daa578235879412925cddca2705da1da
root <rich.buff@gmail.com>
2013-04-14 17:14:19 -0700

    periodic checkin

--- autocompleter.js
+++ autocompleter.js
@@ -1,4 +1,19 @@
 function initPage(ars) {
 //    alert("test");
      $.mobile.hidePageLoadingMsg();
+     
+//     $("#state").bind("click", handleSelectClick);
+//     $("#state").parent().bind("click", function(evt){
+//        $("#popupBasic").popup("open");
+//     });
+     
+     $("#state").easySelect({
+            popupid: "popupBasic"
+        });
+
+}
+
+function handleSelectClick(evt) {
+    evt.preventDefault();
+    alert("test");
 }
